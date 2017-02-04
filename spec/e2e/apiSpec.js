86503d6bdfde32f55eb0b894222118a8b2437957
markwy1 <dr.yongwang@hotmail.com>
2017-02-04 13:42:45 +0800

    first version of apiSpec.js

---
+++ spec/e2e/apiSpec.js
@@ -0,0 +1,16 @@
+/*
+Yong Wang
+From Craftsman.book
+2017.2.4
+p106
+*/
+
+'use strict';
+
+describe('The API', function () {
+
+	it('should just work', function () {
+		expect(true).toBe(true);
+	});
+
+});
